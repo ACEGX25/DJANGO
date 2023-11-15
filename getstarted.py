94914f11e7a44cf7e849b2943c070a65350eef87
Geetesh G. Karjavkar <143728245+ACEGX25@users.noreply.github.com>
2023-11-15 08:56:03 +0530

    introduction

---
+++ getstarted.py
@@ -0,0 +1,12 @@
+#this is the command for running the Django programme (py name.py runserver)
+
+#for creating a project we have the following ways 
+#django-admin startproject Projectname
+#python -m django startproject Projectname
+#py-m django startproject Projectname
+#python3-m django startproject Projectname
+#
+#
+#
+#
+#
